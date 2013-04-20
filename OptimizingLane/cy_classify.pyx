cc01ae9d375ee6e143b70c1e06777ee8d99a1dca
Daniel Smith <smithd@daniel-laptop.(none)>
2013-04-20 15:52:23 -0400

    Updated Cython file with various optimization attempts

--- OptimizingLane/cy_classify.pyx
+++ OptimizingLane/cy_classify.pyx
@@ -34,12 +34,42 @@ def type_classify(float x, float c, float b):
             return 0.5
 
 @cython.boundscheck(False)
-def cy_classify(np.ndarray[np.float_t, ndim=1] data, float c, float b):
+@cython.wraparound(False)
+def cy_classify(double[::1] data, float c, float b):
     cdef unsigned int i
     cdef np.ndarray[np.float_t, ndim=1] out = np.zeros(data.shape[0], 
                                                        dtype=float)
     for i in xrange(data.shape[0]):
         out[i] = c_classify(data[i], c, b)
+    return out
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def cyo_classify(np.ndarray[np.float_t, ndim=1] data, float c, float b):
+    cdef unsigned int i
+    cdef np.ndarray[np.float_t, ndim=1] out = np.zeros(data.shape[0], 
+                                                       dtype=float)
+    for i in xrange(data.shape[0]):
+        out[i] = o_classify(data[i], c, b)
+    return out
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def unr_classify(np.ndarray[np.float_t, ndim=1] data, float c, float b):
+    cdef unsigned int i
+    cdef np.ndarray[np.float_t, ndim=1] out = np.zeros(data.shape[0], 
+                                                       dtype=float)
+    for i in xrange(0, data.shape[0], 8):
+        out[i] = o_classify(data[i], c, b)
+        out[i+1] = o_classify(data[i], c, b)
+        out[i+2] = o_classify(data[i], c, b)
+        out[i+3] = o_classify(data[i], c, b)
+        out[i+4] = o_classify(data[i], c, b)
+        out[i+5] = o_classify(data[i], c, b)
+        out[i+6] = o_classify(data[i], c, b)
+        out[i+7] = o_classify(data[i], c, b)
+    for j in xrange(i+1, data.shape[0]):
+        out[i] = o_classify(data[i], c, b)
     return out
 
 cdef float c_classify(float x, float c, float b):
@@ -56,3 +86,9 @@ cdef float c_classify(float x, float c, float b):
             return (x-c+b)/2/b
         else:
             return 0.5
+
+cdef float o_classify(float x, float m, float w):
+     cdef float out = (x-m)*w
+     out = 0 if (out < 0) else out
+     out = 1 if (out > 1) else out
+     return out
